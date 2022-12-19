4d3786b474b25f2fed29814d7789427abef1c5ca
Roger Grange <rgrang816@west-mec.org>
2022-12-19 12:53:40 -0700

    added resume

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import Landing from "./Landing"
 import About from "./About"
 import Skills from './Skills'
 import Projects from './Projects'
+import Resume from './Resume'
 function Home(){
     return (
     <div class="bg-stone-900  -mt-[50vh] pb-4" style={{'overflow':'hidden'}}>
@@ -12,6 +13,8 @@ function Home(){
     <Skills />
     <hr/>
     <Projects />
+    <hr/>
+    <Resume />
     </div>
     )
 }
---
+++ src/pages/Resume.jsx
@@ -0,0 +1,26 @@
+function Resume(){
+    return <div id="Resume">
+        <h1 class="mx-auto w-fit text-5xl font-bold text-gray-300">Resume</h1>
+        <div class="text-gray-300 w-[100%] sm:w-[66%] mx-auto">
+            <div>
+                <h1 class="text-xl">Roger Grange</h1>
+                <p>CONTACT INFO</p>
+                <p>EMAIL: <a>rgrang816@west-mec.edu</a></p>
+                <p>Phone: <a>(623)-451-2201</a></p>
+            </div>
+            <br/>
+            <div>
+                An Avid backend developer with a strong passion for Game development and a desire
+                to better utilize the languages at their disposal.
+                <br /><br />
+                Highly skilled with the usage of the GODOT game engine, and strong usage of the JAVASCRIPT, PYTHON, and C++ Languages
+                <br /><br />
+                Skilled in finding creative solutions to complex problems and communicating issues and possible causes.
+                <br />
+                Efficient bug fixer and coder.
+            </div>
+        </div>
+    </div>
+}
+
+export default Resume;
